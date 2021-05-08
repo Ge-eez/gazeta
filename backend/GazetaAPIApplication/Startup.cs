45a229c3c8c64737159058e2e2c2569ccaa77d88
Elshadai <elshadaikassutegegn@gmail.com>
2021-05-08 11:41:25 +0300

    Built successfully

--- backend/GazetaAPIApplication/Startup.cs
+++ backend/GazetaAPIApplication/Startup.cs
@@ -34,10 +34,21 @@ namespace GazetaAPIApplication
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
+            if (env.IsDevelopment()){
+                app.UseDeveloperExceptionPage();
+            } else {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>{
+                endpoints.MapRazorPages();
+            });
 
-            app.UseMvc();
+            // app.UseMvc();
         }
     }
 }
