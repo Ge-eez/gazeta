1042fe2d520cc4a078d1c8a658a344c194aa2dc8
Mintesnot Bezabhi <mintesnotbezabhi99@gmail.com>
2021-05-26 23:10:41 +0300

    context for the database added

---
+++ web_frontend/Gazeta/Data/GazetaWebContext.cs
@@ -0,0 +1,37 @@
+﻿using Gazeta.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gazeta.Data
+{
+    public class GazetaWebContext:DbContext
+    {
+        public GazetaWebContext(DbContextOptions<GazetaWebContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<News> News { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Subscription> Subscriptions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<News>().HasKey(x => x.Id);
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>().HasKey(x => x.Email);
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Company>().HasKey(x => x.CompanyName);
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Subscription>().HasKey(x => x.SubscriptionId);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
