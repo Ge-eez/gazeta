e4d69f720bbb98ce05bdaa49e542020612a4ad19
Mintesnot Bezabhi <mintesnotbezabhi99@gmail.com>
2021-05-26 23:09:40 +0300

    table models added

---
+++ web_frontend/Gazeta/Models/Company.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gazeta.Models
+{
+    public class Company
+    {
+        public int CompanyId { get; set; }
+        public string CompanyName { get; set; }
+        public string ProfileImage { get; set; }
+        public string Owner { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ web_frontend/Gazeta/Models/Subscription.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gazeta.Models
+{
+    public class Subscription
+    {
+        public int SubscriptionId { get; set; }
+        public string CompanyName { get; set; }
+        public string UserEmail { get; set; }
+        public string CompanyProfile { get; set; }
+    }
+}
